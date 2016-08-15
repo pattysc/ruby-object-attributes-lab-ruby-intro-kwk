a3c291ae640b2c375a8b97fea74bdec70a002855
p sc <patty.stacruz@gmail.com>
2016-08-15 03:46:41 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name=(doggyname)
+    @name = doggyname
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(doggybreed)
+    @breed = doggybreed
+  end
+
+  def breed
+    @breed
+  end
+end
